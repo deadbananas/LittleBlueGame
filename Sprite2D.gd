cf81e1fcaf45636ea27e2c418858f2ca9b83d465
deadbananas <45050790+deadbananas@users.noreply.github.com>
2024-08-20 20:36:05 -0500

    Attack and Jump
    Implement attack, change jump to new version. Touch up all code to get a solid working baseline.

--- Sprite2D.gd
+++ Sprite2D.gd
@@ -4,12 +4,6 @@ extends AnimatedSprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-func animation_finished():
-	if is_playing():
-		return 0
-	else:
-		return 1
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
--- main_character.gd
+++ main_character.gd
@@ -4,9 +4,19 @@ const UP = Vector2(0,-1)
 const MAX_SPEED = 400 #Max speed in pixels per second
 const ACCELERATION = 100 #acceleration rate
 const FRICTION = 10 #deceleration rate
+
+
+
+
+var jump_height = 200.0
+var jump_time_to_peak = 0.5
+var jump_time_to_descent = 0.39
+
+@onready var jump_velocity = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+@onready var jump_gravity = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+@onready var fall_gravity = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 const minJump = 0 #to make fall instantly if height is lower than this
 const STOP = 30.0
-const JUMP_VELOCITY = -1200.0
 const fall_mod_neg = -5
 const fall_mod_pos = 700
 
@@ -17,9 +27,9 @@ var positionUpdate = Vector2.ZERO
 
 
 var friction = false
-var lastDir = 0
 var jumped = 0
 var attacking = 0
+var mid_attack = 0
 @onready var sprite_2d = $Sprite2D
 @onready var weapon = $Weapon
 
@@ -33,41 +43,38 @@ enum {
 }
 var state = IDLE
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func get_gravity():
+	return jump_gravity if velocity.y < 0 else fall_gravity
 
 func move():
-	move_and_slide()
-	if velocity.x >= 0:
-		lastDir = 0
-		sprite_2d.animation = "right"
-	else:
-		lastDir = 1
-		sprite_2d.animation = "left"
+	if velocity.x > 0:
+		sprite_2d.flip_h = false
+	elif velocity.x < 0:
+		sprite_2d.flip_h = true
+	sprite_2d.play("right")
 func idleAnims():
-	if lastDir == 1:
-		sprite_2d.animation = "default_left"
-	else:
-		sprite_2d.animation = "default"
-func airTime(delta):
-	velocity.y += gravity * delta
+	sprite_2d.play("default")
+func jumpAnims():
 	if velocity.y < fall_mod_neg:
-		sprite_2d.animation = "jump_up"
-	elif fall_mod_neg <= velocity.y and velocity.y <= fall_mod_pos:
-		sprite_2d.animation = "jump_max"
+		sprite_2d.play("jump_up")
+	elif fall_mod_neg <= velocity.y and velocity.y <= fall_mod_pos and !is_on_floor():
+		sprite_2d.play("jump_max")
 	else:
-		sprite_2d.animation = "fall"
+		sprite_2d.play("fall")
 		
 func attack():
-	print("placeholder")
+	if !mid_attack:
+		mid_attack = 1
+		sprite_2d.play("attack_right")
 func _physics_process(delta):
-	playerInput()
+	playerInput(delta)
 	match state:
 		IDLE:
 			idleState()
 		MOVE:
 			moveState()
 		JUMP:
-			jumpState(delta)
+			jumpState()
 		ATTACK:
 			attackState()
 			
@@ -76,32 +83,52 @@ func idleState():
 	
 func moveState():
 	move()
-	
-func jumpState(delta):
-	airTime(delta)
+func jumpState():
+	jumpAnims()
 func attackState():	
 	attack()
 
 					
-func playerInput():
-	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")	
-	if input_vector.x >= 0.15 || input_vector.x <= -0.15:
-		state = MOVE
+func playerInput(delta):
+	
+	if not is_on_floor():
+		velocity.y += get_gravity() * delta
+		state = JUMP
+		input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")	
 		velocity.x = move_toward(velocity.x, input_vector.x*MAX_SPEED, ACCELERATION)
+
 	else:
-		state = IDLE
-	#input_vector.y =  Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-			
-	if Input.is_action_just_pressed("jump"):
-		print("hello")
-		velocity.y = JUMP_VELOCITY
+		input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")	
+		velocity.x = move_toward(velocity.x, input_vector.x*MAX_SPEED, ACCELERATION)
+		if (input_vector.x >= 0.15 || input_vector.x <= -0.15):
+			if !attacking:
+				state = MOVE
+		else:
+			if !attacking:
+				state = IDLE
+
+	if Input.is_action_just_pressed("jump") and is_on_floor() and !attacking:
+		velocity.y = jump_velocity
 		sprite_2d.animation = "jump_up"
 		state = JUMP
-			
-	if Input.is_action_just_released("jump") && velocity.y < minJump:
+
+	if Input.is_action_just_released("jump") && velocity.y < minJump and !attacking:
 		velocity.y = 0
 	
-	if (Input.is_action_just_pressed("attack")):
-		state = ATTACK
+	if (Input.is_action_just_pressed("attack")) and is_on_floor() and !attacking:
+		if !attacking:
+			state = ATTACK
+			attacking = 1
+	if attacking:
+		velocity.x = velocity.x * 0.6
+		velocity.y = 0
+	move_and_slide()
+	
 
+
+
+func _on_sprite_2d_animation_finished():
+	sprite_2d.flip_h = false
+	attacking = 0
+	mid_attack = 0
 
