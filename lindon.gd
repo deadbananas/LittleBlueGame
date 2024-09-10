81228e4a0cf9d83946b9ab22d10623a37ea3802a
deadbananas <45050790+deadbananas@users.noreply.github.com>
2024-09-09 20:33:41 -0500

    Made A test Lindon for animation scale and timing

---
+++ lindon.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@onready var lindon_anim = $LindonAnims
+var play = 0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_timer_timeout():
+	lindon_anim.play("default")
