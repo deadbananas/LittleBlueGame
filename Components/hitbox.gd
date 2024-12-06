db5ae3abc45b39a80ee5dcaf1ff6b301043c1770
deadbananas <45050790+deadbananas@users.noreply.github.com>
2024-12-06 11:52:02 -0600

    Fully Implemented Hitboxes/Hurtboxes and hitstun on all current moves
    Added hitboxes and hurtboxes with hitstun on all current moves by both characters and tested interactions.
    
    TODO: Add registratoin for taking damage, triggering knockback, and flashing the sprite using shaders.

--- Components/hitbox.gd
+++ Components/hitbox.gd
@@ -3,8 +3,8 @@ extends Area2D
 
 
 @export var damage: int = 1 : set = set_damage, get = get_damage
-@export var time_scale: int = 0.05 : set = set_time_scale, get = get_time_scale
-@export var duration: int = 0.4 : set = set_duration, get = get_duration
+@export var time_scale: float = 0.1 : set = set_time_scale, get = get_time_scale
+@export var duration: float = 0.4 : set = set_duration, get = get_duration
 
 
 func set_damage(value: int):
@@ -14,15 +14,15 @@ func get_damage() ->int:
 	return damage
 
 
-func set_time_scale(value: int):
+func set_time_scale(value: float):
 	time_scale = value
 
-func get_time_scale() ->int:
+func get_time_scale() ->float:
 	return time_scale
 
 
-func set_duration(value : int):
+func set_duration(value : float):
 	duration = value
 	
-func get_duration() -> int:
+func get_duration() -> float:
 	return duration
--- Components/hurtbox.gd
+++ Components/hurtbox.gd
@@ -3,21 +3,39 @@ extends Area2D
 
 
 
-signal received_hit(damage: int, time_scale: int, duration: int)
+signal received_hit(damage: int, time_scale: float, duration: float)
+
+
+var hitable = true
+
+@export var hit_immunity = 0.7
 
 #@export var health: Health
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	connect("area_entered", _on_area_entered)
-
-
-func _on_area_entered(hitbox: Hitbox) -> void:
+	
+func _on_area_entered(hitbox: HitBox) -> void:
+	print("on cooldown")
 	if hitbox != null:
-		frameFreeze(hitbox.time_scale, hitbox.duration)
-		received_hit.emit(hitbox.damage, hitbox.time_scale, hitbox.duration)
+		if hitable:
+			print("hit")
+			frameFreeze(hitbox.time_scale, hitbox.duration)
+			received_hit.emit(hitbox.damage, hitbox.time_scale, hitbox.duration)
+			var hitTimer : Timer = Timer.new()
+			add_child(hitTimer)
+			hitTimer.one_shot = true
+			hitTimer.autostart = true
+			hitTimer.wait_time = hit_immunity
+			hitTimer.timeout.connect(timer_timeout)
+			hitTimer.start()
+			hitable = false
 		
 func frameFreeze(time_scale, duration):
 	Engine.time_scale = time_scale
 	await(get_tree().create_timer(time_scale * duration).timeout)
 	Engine.time_scale = 1.0
+
+func timer_timeout():
+	hitable = true
